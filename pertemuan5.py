93f1cc3918cc6f3b81c4eda870f620c020cd9ede
mikailmusa <muha23030ti@student.nurulfikri.ac.id>
2023-10-29 21:56:01 +0700

    perbaikan match case

--- pertemuan5.py
+++ pertemuan5.py
@@ -6,17 +6,24 @@ motor.append("tipe150cc")
 motor.insert(2,"Yamaha")
 print(motor)
 
+luas = 0
 print("Kalkulator luas\n1. Luas Persegi\n2. Luas lingkaran\n3. Luas segi tiga")
 pilihan = int(input("Pilih perhitungan luas: "))
 match pilihan:
-    case 1 :
+    case 1:
         s = int(input("masukan panjang sisi: "))
         luas = s*s
-    case 2 :
+        print("luas persegi adalah: ",luas)
+    case 2:
         r = int(input("masukan jari jari: "))
         luas = 3.14*r*r
-    case 3 :
+        print("luas lingkaran adalah: ",luas)
+    case 3:
          t = int(input("masukan tinggi segi tiga: "))
          a = int(input("masukan alas segi tiga: "))
          luas = 1/2*a*t
-print("luas nya adalah: ",luas)
+         print("luas segi tiga adalah: ",luas)
+    case _:
+        print("perhitungan tidak valid, coba masukan pilihan dengan angka 1 sampai 3")
+
+ 
